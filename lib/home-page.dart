caaeae66691559ffeb0dd50af67a1221c863a400
faiza0336 <faizaawan336@gmail.com>
2022-04-02 17:25:07 +0500

    remove lines

--- lib/home-page.dart
+++ lib/home-page.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:socialapp/routes.dart';
-//
-//
-//
-//
-//
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
